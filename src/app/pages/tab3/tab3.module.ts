2e558c591fa56faf04847e0e5cec14e109773a49
Raul Lourenço <raulourenco@hotmail.com>
2020-05-03 18:19:40 -0300

    creating profile page

--- src/app/pages/tab3/tab3.module.ts
+++ src/app/pages/tab3/tab3.module.ts
@@ -1,11 +1,12 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module'
+import { ProfileModule } from './components/profile/profile.module';
 
 @NgModule({
   imports: [
@@ -14,7 +15,9 @@ import { Tab3PageRoutingModule } from './tab3-routing.module'
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
+    ProfileModule
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab3PageModule {}
