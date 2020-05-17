b60172035e06e95926e3573ca8a686b47055d75b
Raul Lourenço <raulourenco@hotmail.com>
2020-05-17 17:58:24 -0300

    create signup page

--- src/app/pages/index-login/signup/signup.page.ts
+++ src/app/pages/index-login/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, LoadingController } from '@ionic/angular'
+import { FormBuilder, Validators, FormGroup } from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  public userForm: FormGroup;
+  public buttonText: string;
+
+  constructor(private alertController: AlertController,
+    private loadingController: LoadingController,
+    private formBuilder: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
+    this.initializeForm();
+  }
+
+  private async onSignup(cpf, password) {
+    if (cpf == '123' && password == '123') {
+      this.router.navigateByUrl('home/tabs/tab1');
+    } else {
+      return 'loucura bicho';
+    }
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Aguarde....'
+    });
+    return await loading.present();
+  }
+
+  async dismissLoading() {
+    return await this.loadingController.dismiss();
+  }
+
+  async presentAlert(message) {
+    const alertPresent = await this.alertController.create({
+      message: message
+    });
+    return await alertPresent.present();
+  }
+
+  public backToIndex(){
+    this.router.navigateByUrl('/index-login');
+  }
+
+  initializeForm() {
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      user: ['', Validators.required],
+      password: ['', Validators.required],
+      repeatPassword: ['', Validators.required]
+    });
   }
 
 }
