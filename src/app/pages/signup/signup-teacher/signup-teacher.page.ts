70598eca4b1066dbaa80f6f9883eaab882fbf88c
t719917 <raul.dos.santos@santander.com.br>
2020-05-24 18:32:07 -0300

    create signup teacher

--- src/app/pages/signup/signup-teacher/signup-teacher.page.ts
+++ src/app/pages/signup/signup-teacher/signup-teacher.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup-teacher',
   templateUrl: './signup-teacher.page.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupTeacherPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  closeTeacherSignup() {
+    this.router.navigateByUrl('home/tabs/tab3');
+  }
 }
