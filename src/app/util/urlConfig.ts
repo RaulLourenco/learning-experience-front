5b6051d4d947c89de9f4a98f2b4495ba8fd3ab07
t719917 <raul.dos.santos@santander.com.br>
2020-05-23 22:17:05 -0300

    add url getUserById

--- src/app/util/urlConfig.ts
+++ src/app/util/urlConfig.ts
@@ -2,4 +2,5 @@ export const urls = {
     URL_API: 'http://learningexperieceapi.azurewebsites.net/api/v1',
     URL_GETALLADVISOR: 'https://learningexperieceapi.azurewebsites.net/api/v1/Advisor/GetAll',
     URL_GETALLPATIENT: 'https://learningexperieceapi.azurewebsites.net/api/v1/Patient/GetAll',
-}
+    URL_GETUSERBYID: 'https://learningexperieceapi.azurewebsites.net/api/v1/User/GetUserById'
+};
