00b0dd270d18198535dee2ac2da4902b74d0d81b
t719917 <raul.dos.santos@santander.com.br>
2020-05-25 02:10:23 -0300

    merge ft/http

--- src/app/pages/exercise-page/exercise-page.page.ts
+++ src/app/pages/exercise-page/exercise-page.page.ts
@@ -16,7 +16,7 @@ export class ExercisePagePage implements OnInit {
   }
 
   public closeExercise(){
-    this.router.navigateByUrl('/home/tabs/tab1');
+    this.router.navigateByUrl('/home/tab1');
   }
 
 }
