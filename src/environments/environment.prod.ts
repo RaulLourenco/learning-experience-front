9e48f7a7d5351d3c4bd78df8772ed82522fa65a5
Raul Lourenco <raulourenco@hotmail.com>
2020-10-12 13:02:35 -0300

    some adjusts at home page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlApi: 'http://165.227.62.217:5001/api/v1'
+  urlApi: 'http://167.172.213.118/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlApi: 'http://localhost:5000/api/v1'
+  urlApi: 'http://167.172.213.118/api/v1'
 };
 
 /*
